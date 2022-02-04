7f7d3501348785ba46bd19130242ef6767f35509
Leo Allnutt <festfolk@hotmail.co.uk>
2022-02-04 20:57:15 +0000

    made a start on the teams array

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,1 +1,5 @@
-const quote: ['He who dares wins' 'YOLO'];
+const teams = ['Arsenal', 'Aston Villa', 'Barnsley', 'Birmingham City',
+'Blackburn Rovers', 'Blackpool', 'Bolton Wanderers', 'Bournemouth', 'Bradford City'
+,'Brentford', 'Brighton & Hove Albion', 'Burnley', 'Cardiff City', 
+'Charlton Athletic', 'Chelsea', 'Coventry City'];
+
