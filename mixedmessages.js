7f35f733be999defd6c1022f749994f7f35394a0
Leo Allnutt <festfolk@hotmail.co.uk>
2022-02-07 11:14:32 +0000

    added all variables required to run program, now figuiring out the function

--- mixedmessages.js
+++ mixedmessages.js
@@ -7,4 +7,25 @@ const teams = ['Arsenal', 'Aston Villa', 'Barnsley', 'Birmingham City',
   'Middlesbrough', 'Newcastle United', 'Norwich City', 'Nottingham Forest', 
   'Oldham Athletic', 'Portsmouth', 'Queens Park Rangers', 'Reading', 'Sheffield United'];
  
+const gameTimes = ['12:30', '16:30', '15:00', '17:00', '17:30', '20:00'];
 
+const teamAScore = Math.floor((Math.random() * 6) + 1);
+const teamBScore = Math.floor((Math.random() * 6) + 1);
+
+
+
+const getTeamA = teams[Math.floor(Math.random()*teams.length)];
+const getTeamB = teams[Math.floor(Math.random()*teams.length)];
+
+
+//if team a === team b = +1
+
+function generateScore () {
+    return getTeamA + getTeamB;
+    if (getTeamA === getTeamB) {
+        return getTeamA +1;
+    }
+    
+};
+
+console.log(generateScore());
