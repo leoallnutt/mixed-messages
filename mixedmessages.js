eeba4b1ae0eb773a3134990d74240bb50a26898b
Leo Allnutt <festfolk@hotmail.co.uk>
2022-02-07 10:21:09 +0000

    finished teams array

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,5 +1,10 @@
 const teams = ['Arsenal', 'Aston Villa', 'Barnsley', 'Birmingham City',
 'Blackburn Rovers', 'Blackpool', 'Bolton Wanderers', 'Bournemouth', 'Bradford City'
 ,'Brentford', 'Brighton & Hove Albion', 'Burnley', 'Cardiff City', 
-'Charlton Athletic', 'Chelsea', 'Coventry City'];
+'Charlton Athletic', 'Chelsea', 'Coventry City', 'Crystal Palace', 
+'Derby County', 'Everton', 'Fulham', 'Huddersfield Town', 'Hull City', 'Ipswich Town',
+ 'Leeds United', 'Leicester City', 'Liverpool', 'Manchester City', 'Manchester United',
+  'Middlesbrough', 'Newcastle United', 'Norwich City', 'Nottingham Forest', 
+  'Oldham Athletic', 'Portsmouth', 'Queens Park Rangers', 'Reading', 'Sheffield United'];
+ 
 
