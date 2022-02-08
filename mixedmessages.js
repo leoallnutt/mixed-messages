3a05fa8ce9b9c41c48e09f0dda52995b3b733bdf
Leo Allnutt <festfolk@hotmail.co.uk>
2022-02-08 10:48:26 +0000

    Completed the majority of the programme

--- mixedmessages.js
+++ mixedmessages.js
@@ -8,9 +8,10 @@ const teams = ['Arsenal', 'Aston Villa', 'Barnsley', 'Birmingham City',
   'Oldham Athletic', 'Portsmouth', 'Queens Park Rangers', 'Reading', 'Sheffield United'];
  
 const gameTimes = ['12:30', '16:30', '15:00', '17:00', '17:30', '20:00'];
+let time = gameTimes[Math.floor(Math.random()*gameTimes.length)];
 
-const teamAScore = Math.floor((Math.random() * 6) + 1);
-const teamBScore = Math.floor((Math.random() * 6) + 1);
+const teamAScore = Math.floor((Math.random() * 4) + 1);
+const teamBScore = Math.floor((Math.random() * 4) + 1);
 
 
 
@@ -18,14 +19,13 @@ const getTeamA = teams[Math.floor(Math.random()*teams.length)];
 const getTeamB = teams[Math.floor(Math.random()*teams.length)];
 
 
-//if team a === team b = +1
 
 function generateScore () {
-    return getTeamA + getTeamB;
     if (getTeamA === getTeamB) {
-        return getTeamA +1;
+        return `${teams[0]} ${teamAScore} - ${teams[1]} ${teamBScore} / Time: ${time}`;
+    } else { 
+        return `${getTeamA} ${teamAScore} - ${getTeamB} ${teamBScore} / Time: ${time}`;
     }
-    
 };
 
 console.log(generateScore());
